09708fddd92f8a538ebb52ed8e1148397bc1b083
YuvanX <abhivigneshofficial@gmail.com>
2025-06-26 14:25:59 +0530

    fix: Landing Toggle layout fxi

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { BackgroundPaths } from "@/components/ui/background-paths"
 export default function Home() {
   return (
     <div className="relative">
-      <div className="absolute right-20 top-10 z-50">
+      <div className="absolute top-10 right-10 md:right-20 md:top-10 z-50">
          <ModeToggle/>
       </div>
       <BackgroundPaths title="Nexus" />
